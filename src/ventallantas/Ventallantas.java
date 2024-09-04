8077143f71a6295771254e530e971f75f0782e25
Ingssanchez <ingssanchez@misena.edu.co>
2024-09-03 23:21:39 -0500

    Update from Computer
    Versión Netbeans: Apache NetBeans IDE 22
    Versión de Java: java version "17.0.11" 2024-04-16 LTS
    Java(TM) SE Runtime Environment (build 17.0.11+7-LTS-207)
    Java HotSpot(TM) 64-Bit Server VM (build 17.0.11+7-LTS-207, mixed mode, sharing)

---
+++ src/ventallantas/Ventallantas.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+/**
+ *
+ * @author Sergio Sanchez 20241222424 Celular 3208689548
+ */
+package ventallantas; // El nombre del paquete debe ser igual al nombre de la clase pública
+
+import java.util.Scanner;
+
+public class Ventallantas {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Ingrese la cantidad de llantas a comprar: ");
+        int cantidadLlantas = scanner.nextInt();
+
+        double precioUnitario = 0.0;
+        double precioTotal = 0.0;
+
+        // Condiciones utilizadas en el programa
+        if (cantidadLlantas < 5) {
+            precioUnitario = 100.0;
+        } else if (cantidadLlantas >= 5 && cantidadLlantas <= 10) {
+            precioUnitario = 75.0;
+        } else { // Más de 10 llantas
+            precioUnitario = 50.0;
+        }
+
+        precioTotal = cantidadLlantas * precioUnitario;
+
+        System.out.println("Precio por llanta: COP $" + precioUnitario);
+        System.out.println("Precio total de la compra: COP $" + precioTotal);
+
+        scanner.close();
+    }
+}
